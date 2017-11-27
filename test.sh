1310a2217ac600d9422a0f7592c65522ae5e0878
DontGoChasingWaterfall <33838508+DontGoChasingWaterfall@users.noreply.github.com>
2017-11-27 18:20:30 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@
 script_output="$(bash bash_agile.sh)"
 regex="^$(whoami): [0-9]{4}-[0-9]{2}-[0-9]{2}\$"
 if [[ $script_output =~ $regex ]]; then
-    echo "Build Success: Program gave expected output."
+    echo "Test Passed: Program gave expected output."
     exit 0
 else
-    echo "Build Failure: Program failed to match expected output." >&2
+    echo "Test Failed: Program failed to match expected output." >&2
     echo "Input text: $script_output" >&2
     echo "Regex: $regex" >&2
     exit 1
