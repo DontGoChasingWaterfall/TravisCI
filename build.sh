5cd36ebc6b2dce21ddca660c6608fe9fc2b9b7b3
DontGoChasingWaterfall <33838508+DontGoChasingWaterfall@users.noreply.github.com>
2017-11-27 13:58:57 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_output="sh ./bash_agile.sh"
+regex="^$(whoami): [0-9]{4}-[0-9]{2}-[0-9]{2}\$"
+if [[ !$script_output =~ $regex ]]; then
+    echo "Build Success: Program gave expected output."
+    exit 0
+else
+    echo "Build Failure: Program failed to match expected output." >&2
+    echo "Input text: $script_output" >&2
+    echo "Regex: $regex" >&2
+    exit 1
+fi
