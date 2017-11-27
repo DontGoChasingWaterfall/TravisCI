390ed9708f7570b7b5486e4d0f5e9c5f843ebb3c
DontGoChasingWaterfall <33838508+DontGoChasingWaterfall@users.noreply.github.com>
2017-11-27 14:10:08 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 script_output="$(bash ./bash_agile.sh)"
 regex="^$(whoami): [0-9]{4}-[0-9]{2}-[0-9]{2}\$"
-if [[ !$script_output =~ $regex ]]; then
+if [[ $script_output =~ $regex ]]; then
     echo "Build Success: Program gave expected output."
     exit 0
 else
